bfeb15b0a1a64c6951d669a52e14b457eeb885e6
kothariji <kotharidhruv25@gmail.com>
2021-09-14 23:06:43 +0530

    added a baar

--- src/App.js
+++ src/App.js
@@ -38,6 +38,7 @@ const App = () => {
       <br />
       <br />
       <br />
+      <br />
       <label htmlFor="deci">Decimal:</label>
       <input data-testid='deci-input' id="deci" type="text" value={decimalValue} onChange={handleDecimalChange}></input>
     </div>
