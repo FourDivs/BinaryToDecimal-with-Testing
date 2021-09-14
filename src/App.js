f6a4758ddc737b624612b3e245fccaab155984db
kothariji <kotharidhruv25@gmail.com>
2021-09-14 14:49:58 +0530

    added testing code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [bcdValue, setBcdValue] = useState('');
+  const [decimalValue, setdecimalValue] = useState('');
+
+  const handleBCDChange = (event) => {
+    setBcdValue(event.target.value);
+    setdecimalValue(convertBCDToDecimal(event.target.value));
+  }
+
+  const handleDecimalChange = (event) => {
+    setdecimalValue(event.target.value);
+    setBcdValue(convertDecimalToBCD(event.target.value));
+  }
+
+  //convert BCD to decimal
+  const convertBCDToDecimal = (bcdValue) => {
+    return parseInt(bcdValue, 2).toString(10);
+  }
+
+  const convertDecimalToBCD = (decimalValue) => {
+    return parseInt(decimalValue).toString(2);
+  }
+  //convert decimal to binary
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Binary to Decimal Converter</h1>
+      <label htmlFor="bcd">Binary:</label>
+      <input id="bcd" data-testid='binary-input' type="text" value={bcdValue} onChange={handleBCDChange}></input>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <label htmlFor="deci">Decimal:</label>
+      <input data-testid='deci-input' id="deci" type="text" value={decimalValue} onChange={handleDecimalChange}></input>
     </div>
-  );
+  )
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,40 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from '@testing-library/user-event';
 
-test('renders learn react link', () => {
+test('renders Binary to Decimal Converter', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Binary to Decimal Converter/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('decimal input Check', () => {
+  render(<App />);
+  const linkElement = screen.getByTestId('deci-input');
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('binary input Check', () => {
+  render(<App />);
+  const linkElement = screen.getByTestId('binary-input');
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('binary to decimal Check', () => {
+  render(<App />);
+  const BinaryElement = screen.getByTestId('binary-input');
+  const DeciElement = screen.getByTestId('deci-input');
+
+  userEvent.type(BinaryElement, "101");
+  expect(DeciElement.value).toBe("5");
+});
+
+test('decimal to binary Check', () => {
+  render(<App />);
+  const BinaryElement = screen.getByTestId('binary-input');
+  const DeciElement = screen.getByTestId('deci-input');
+
+  userEvent.type(DeciElement, "13");
+  expect(BinaryElement.value).toBe("1101");
+});
+
